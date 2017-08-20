34bf71766d1d97b75ceadef23bf174c1b5bc449b
Ivan Liljeqvist <ivanli@kth.se>
2017-08-20 20:13:55 +0200

    Initial Commit

---
+++ CoinMarketCapClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoinMarketCapClone
+//
+//  Created by Ivan Liljeqvist on 2017-08-20.
+//  Copyright © 2017 Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
